51899565bd0fb1ca167652e3c30bbd53630af8aa
nikitaafe <nikitafelicia88@gmail.com>
2022-07-20 11:11:31 +0700

    Initial Commit

---
+++ LiNotes/LiNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiNotesApp.swift
+//  LiNotes
+//
+//  Created by Nikita Felicia on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LiNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
